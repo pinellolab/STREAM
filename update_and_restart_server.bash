ea85aa2660eefeddea5190d960d158616e02ea86
Luca Pinello <lucapinello@gmail.com>
2018-04-22 09:20:16 -0400

    update script

--- update_and_restart_server.bash
+++ update_and_restart_server.bash
@@ -2,7 +2,7 @@ git pull
 docker stop $(docker ps -q --filter ancestor=pinellolab/stream)
 docker run -p 10001:10001 \
 -v /Users/sailor/Projects/STREAM/STREAM:/STREAM \
--v /Volumes/pinello/PROJECTS/2016_12_STREAM/STREAM_DB_top_15/:/STREAM/precomputed \
+#-v /Volumes/pinello/PROJECTS/2016_12_STREAM/STREAM_DB_top_15/:/STREAM/precomputed \
 -d -it pinellolab/stream STREAM_webapp
 
 
@@ -11,4 +11,6 @@ docker run -p 10001:10001 \
 
 #-v /Volumes/Data/STREAM/tmp:/tmp \
 #-v /Volumes/Data/STREAM_DB_top_15/:/STREAM/precomputed \
+#-v /Volumes/pinello/PROJECTS/2016_12_STREAM/STREAM_DB_top_15/:/STREAM/precomputed \
+
 
