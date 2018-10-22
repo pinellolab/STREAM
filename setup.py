2f10d29d41b26d8026b84c4c6cd9bf8e4b70dc6b
huidongchen <gjlmile@gmail.com>
2018-10-22 11:58:03 -0400

    update version info

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if sys.version_info.major != 3:
 
 
 setup(name='stream',
-      version="0.2.0",
+      version="0.2.6",
       description='Single-cell Trajectories Reconstruction, Exploration And Mapping of single-cell data',
       url='https://github.com/pinellolab/stream',
       author='Huidong Chen',
