07ff005b80d1714b9acd59aeea36a341b1470243
huidongchen <hd7chen@gmail.com>
2020-05-19 01:29:50 -0400

    update read();remove parameter 'experiment'

--- stream/core.py
+++ stream/core.py
@@ -112,30 +112,51 @@ def set_workdir(adata,workdir=None):
     print('Saving results in: %s' % workdir)
 
 
-def read(file_name,file_path='',file_format='tsv',delimiter='\t',experiment='rna-seq', workdir=None,**kwargs):
+def read(file_name,file_path='',file_format='tsv',delimiter='\t',workdir=None, file_sample='barcodes.tsv',file_feature='genes.tsv',**kwargs):
     """Read gene expression matrix into anndata object.
     
     Parameters
     ----------
     file_name: `str`
-        File name. For atac-seq data, it's the z-score matrix file name.
+        Input data file name.
     file_path: `str`, optional (default: '')
-        File path. By default it's empty
+        File path. 
     file_format: `str`, optional (default: 'tsv')
         File format. currently supported file formats: 'tsv','txt','tab','data','csv','mtx','h5ad','pklz','pkl'
     delimiter: `str`, optional (default: '\t')
-        Delimiter to use.
-    experiment: `str`, optional (default: 'rna-seq')
-        Choose from {{'rna-seq','atac-seq'}}       
+        Delimiter to use.    
     workdir: `float`, optional (default: None)
         Working directory. If it's not specified, a folder named 'stream_result' will be created under the current directory
+    file_sample:`str`, optional (default: 'barcodes.tsv')
+        Sample file name. Only valid when file_format = 'mtx'
+    file_feature:`str`, optional (default: 'genes.tsv')
+        Feature file name. Only valid when file_format = 'mtx'
     **kwargs: additional arguments to `Anndata` reading functions
    
     Returns
     -------
     AnnData object
     """       
-    _fp = lambda f:  os.path.join(file_path,f)
+    assert (file_format in ['tsv','txt','tab','data','csv','mtx','h5ad','pkl','pklz']),"file_format must be one of ['tsv','txt','tab','data','csv','mtx','h5ad','pkl','pklz']"
+    _fp = lambda f:  os.path.join(file_path,f)     
+    if(file_format in ['tsv','txt','tab','data']):
+        adata = ad.read_text(_fp(file_name),delimiter=delimiter,**kwargs).T
+        # adata.raw = adata        
+    if(file_format == 'csv'):
+        adata = ad.read_csv(_fp(file_name),delimiter=delimiter,**kwargs).T
+        # adata.raw = adata
+    if(file_format == 'mtx'):
+        adata = ad.read_mtx(_fp(file_name),**kwargs).T 
+        adata.X = np.array(adata.X.todense())
+        print(_fp(os.path.join(os.path.dirname(file_name),file_feature)))
+        genes = pd.read_csv(_fp(os.path.join(os.path.dirname(file_name),file_feature)), header=None, sep='\t')
+        adata.var_names = genes[1]
+        adata.var['gene_ids'] = genes[0].values
+        print(_fp(os.path.join(os.path.dirname(file_name),file_sample)))
+        adata.obs_names = pd.read_csv(_fp(os.path.join(os.path.dirname(file_name),file_sample)), header=None)[0]
+        # adata.raw = adata
+    if(file_format == 'h5ad'):
+        adata = ad.read_h5ad(_fp(file_name),**kwargs)
     if(file_format == 'pkl'):
         if file_name.split('.')[-1]=='gz':
             f = gzip.open(_fp(file_name), 'rb')
@@ -145,36 +166,10 @@ def read(file_name,file_path='',file_format='tsv',delimiter='\t',experiment='rna
             f = open(_fp(file_name), 'rb')
             adata = pickle.load(f)
             f.close()    
-    elif(file_format == 'pklz'):
+    if(file_format == 'pklz'):
         f = gzip.open(_fp(file_name), 'rb')
         adata = pickle.load(f)
         f.close()
-    else:
-        if(experiment not in ['rna-seq','atac-seq']):
-            print('The experiment '+experiment +' is not supported')
-            return         
-        if(file_format in ['tsv','txt','tab','data']):
-            adata = ad.read_text(_fp(file_name),delimiter=delimiter,**kwargs).T
-            # adata.raw = adata        
-        elif(file_format == 'csv'):
-            adata = ad.read_csv(_fp(file_name),delimiter=delimiter,**kwargs).T
-            # adata.raw = adata
-        elif(file_format == 'mtx'):
-            adata = ad.read_mtx(_fp(file_name),**kwargs).T 
-            adata.X = np.array(adata.X.todense())
-            print(_fp(os.path.join(os.path.dirname(file_name),'genes.tsv')))
-            genes = pd.read_csv(_fp(os.path.join(os.path.dirname(file_name),'genes.tsv')), header=None, sep='\t')
-            adata.var_names = genes[1]
-            adata.var['gene_ids'] = genes[0].values
-            print(_fp(os.path.join(os.path.dirname(file_name),'barcodes.tsv')))
-            adata.obs_names = pd.read_csv(_fp(os.path.join(os.path.dirname(file_name),'barcodes.tsv')), header=None)[0]
-            # adata.raw = adata
-        elif(file_format == 'h5ad'):
-            adata = ad.read_h5ad(_fp(file_name),**kwargs)
-        else:
-            print('file format ' + file_format + ' is not supported')
-            return
-        adata.uns['experiment'] = experiment
                 
     if('workdir' not in adata.uns_keys()):
         set_workdir(adata,workdir=workdir)
