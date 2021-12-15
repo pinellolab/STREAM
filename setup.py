0844f7bccd809c55558d1bff4f4d14036b810667
huidongchen <hd7chen@gmail.com>
2021-12-15 16:31:16 -0500

    update stream to v1.1

--- setup.py
+++ setup.py
@@ -9,23 +9,26 @@ if sys.version_info.major != 3:
     raise RuntimeError('STREAM requires Python 3')
 
 
-#No dependency packages are specified in setup.py file
-#STREAM is built based on bioconda recipe. All the required packages can be found in the recipe file 
-#https://github.com/bioconda/bioconda-recipes/tree/master/recipes/stream
+# No dependency packages are specified in setup.py file
+# STREAM is built based on bioconda recipe.
+# All the required packages can be found in the recipe file
+# https://github.com/bioconda/bioconda-recipes/tree/master/recipes/stream
 
 setup(name='stream',
       version=st.__version__,
-      description='Single-cell Trajectories Reconstruction, Exploration And Mapping of single-cell data',
+      description=('Single-cell Trajectories Reconstruction,'
+                   ' Exploration And Mapping of single-cell data'),
       long_description=Path('README.md').read_text('utf-8'),
       url='https://github.com/pinellolab/stream',
       author='Huidong Chen',
       author_email='huidong.chen AT mgh DOT harvard DOT edu',
       license='AGPL-3',
       packages=['stream'],
-      package_dir={'stream':'stream'},
+      package_dir={'stream': 'stream'},
       package_data={'stream': ['tests/*']},
-      include_package_data = True,
+      include_package_data=True,
       install_requires=[''],
-      entry_points = {'console_scripts': ['stream_run_test=stream.tests.stream_run_test:main']}
+      entry_points={
+          'console_scripts': [
+              'stream_run_test=stream.tests.stream_run_test:main']}
       )
-
--- stream/__init__.py
+++ stream/__init__.py
@@ -1,3 +1,3 @@
 from .core import *
 
-__version__ = "1.0"
+__version__ = "1.1"
